8730995784f7b54588c9c55291d1772eaa3f494e
Andrew <andrewmfabbro@gmail.com>
2023-03-04 17:53:20 -0500

    Made card descriptions and a banner.

--- src/App.js
+++ src/App.js
@@ -8,19 +8,36 @@ import CortanaImage from './images/cortana.png'
 function App() {
     return(
         <div>
-            <div>Personal Digital Assistants</div>
+            <section className='hero is-primary'>
+                <div className='hero-body'>
+                    <p className='title'>Personal Digital Assistants</p>
+                </div>
+            </section>
 
             <div className='container'>
                 <section className="section">
                     <div className='columns'>
                         <div className='column is-4'>
-                            <ProfileCard title="Alexa" handle="@alexa94" image={AlexaImage}/>
+                            <ProfileCard 
+                            title="Alexa" 
+                            handle="@alexa94" 
+                            image={AlexaImage}
+                            description="Alexa was created by Amazon and buys you stuff from Jeff. She also plays music, videos, and answers the door."
+                            />
                         </div>
                         <div className='column is-4'>
-                            <ProfileCard title="Cortana" handle="@cortana01" image={CortanaImage}/>
+                            <ProfileCard title="Cortana" 
+                            handle="@cortana01" 
+                            image={CortanaImage}
+                            description="Cortana was created by Dr.Halsey (and Microsoft) and is Master Chief's AI assistant."
+                            />
                         </div>
                         <div className='column is-4'>
-                            <ProfileCard title="Siri" handle="@siri11" image={SiriImage}/>
+                            <ProfileCard title="Siri" 
+                            handle="@siri11" 
+                            image={SiriImage}
+                            description="Siri was made by Apple she wakes when you don't need her to help you. She also plays music."
+                            />
                         </div>
                     </div>
                 </section>
